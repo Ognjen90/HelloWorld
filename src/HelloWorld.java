9da7adef5ee5aadac13abe2dd3c9a68a8764d011
Ognjen <ognjenma90@gmail.com>
2020-02-19 21:21:45 +0100

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld!!!");
+		System.out.print("testing");
+		System.out.print(" aplication");
+		System.out.println(" for selenium");
+		System.out.println("testing aplication for selenium");
+		System.out.println("save with CTRL = CTRL + f11");
+		System.out.println(23);
+	}
+
+}
